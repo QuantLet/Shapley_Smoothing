697c0f27ce1c95ed41f42fe0c5800d67c0122769
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2022-11-10 12:16:24 +0100

    Delete sim_consistenc/helper directory

--- sim_consistenc/helper/SE_vec.R
+++
@@ -1,6 +0,0 @@
-
-SE_vec = function(j, x_eval){
-  return( (shapley(j, x_eval) - shapley_popul(j, x_eval))^2 )
-}
-
-
--- sim_consistenc/helper/SE_vec_int.R
+++
@@ -1,6 +0,0 @@
-
-SE_vec_int = function(j, x_eval){
-  return( (shapley_int(j, x_eval) - shapley_popul(j, x_eval))^2 ) 
-}
-
-
