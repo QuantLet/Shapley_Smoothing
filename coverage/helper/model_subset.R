a7146a7eba3df31a836a59ce9dbb72365411a6a8
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2022-11-11 13:22:23 +0100

    Add files via upload

---
+++ coverage/helper/model_subset.R
@@ -0,0 +1,15 @@
+# start function: abh. von X,Y, subsets
+
+model_subset = function(X, Y, subset_names){
+  dat = data.frame(Y,X)
+  Xnam = subset_names
+ 
+  model.npp = npreg(reformulate(Xnam, "Y"), regtype = "ll", data = dat, cfac.init=1, 
+                    initc.dir=1, remin=FALSE, bwscaling=FALSE, bwmethod="cv.aic", nmulti=0)
+  
+
+  
+  return(model.npp)
+}
+
+
